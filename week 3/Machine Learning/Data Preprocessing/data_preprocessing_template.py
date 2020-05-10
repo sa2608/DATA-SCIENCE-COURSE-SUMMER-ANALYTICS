797e3541a184f4982020e5be1631e058a0712e70
sanikumar sahani <sanikumarsahani@gmail.com>
2020-05-10 19:15:28 +0530

    Update data_preprocessing_template.py
    comments

--- week 3/Machine Learning/Data Preprocessing/data_preprocessing_template.py
+++ week 3/Machine Learning/Data Preprocessing/data_preprocessing_template.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, :-1].values     #to create the matrix of the variables (take all the columns except the last column)
+y = dataset.iloc[:, 3].values       # to create the matrix of the variables(to select the last column)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
